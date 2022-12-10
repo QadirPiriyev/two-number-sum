49d82f977f58d3016a58ff29ee2b9a2273cdcf8d
QadirPiriyev <qadirpiriyev@yahoo.com>
2022-12-11 01:39:26 +0400

    repo from console

---
+++ twoNumberSum/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace twoNumberSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+                Console.WriteLine("zehmet olmasa 1 eded daxil edin");
+                int a = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("zehmet olmasa 1 eded daha daxil edin");
+                int b = Convert.ToInt32(Console.ReadLine());
+                int cavab = a + b;
+                Console.WriteLine("bu ededlerin cemi: " + cavab);
+            }
+
+        }
+    }
+}
